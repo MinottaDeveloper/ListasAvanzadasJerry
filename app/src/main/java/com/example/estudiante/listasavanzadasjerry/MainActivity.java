4a92f3693f460430aea44a853cc29b8de36f0f86
juan david losada <juandavidlosada2@gmail.com>
2018-08-31 15:25:20 -0500

    final de listas

--- app/src/main/java/com/example/estudiante/listasavanzadasjerry/MainActivity.java
+++ app/src/main/java/com/example/estudiante/listasavanzadasjerry/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lv_noticias;
     NoticiaAdapter customAdapter;
+    private Button btn_generar;
+    private EditText et_ingresarnoti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         lv_noticias= findViewById(R.id.lv_noticias);
 
+        btn_generar= findViewById(R.id.btn_generar);
+        et_ingresarnoti= findViewById(R.id.et_ingresarnoti);
+
         customAdapter= new com.example.estudiante.listasavanzadasjerry.NoticiaAdapter(this);
 
         lv_noticias.setAdapter(customAdapter);
+
+
+
 
 
         Noticia noticia1;
@@ -35,6 +47,28 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 Noticia noticia_click = (Noticia) customAdapter.getItem(position);
                 Toast.makeText( MainActivity.this,noticia_click.getDescripcion(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_generar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Calendar c = Calendar.getInstance();
+                int year= c.get(Calendar.YEAR);
+                int month= c.get(Calendar.MONTH);
+                month++;
+                int day= c.get(Calendar.DAY_OF_MONTH);
+
+                String fecha= day+"/"+month+"/"+year;
+
+
+
+
+                String titulo = et_ingresarnoti.getText().toString() ;
+                Noticia newNoticia = new Noticia(titulo, "aqui va la descripcion", fecha);
+                customAdapter.agregar(newNoticia);
+                et_ingresarnoti.setText("");
             }
         });
     }
--- app/src/main/java/com/example/estudiante/listasavanzadasjerry/NoticiaAdapter.java
+++ app/src/main/java/com/example/estudiante/listasavanzadasjerry/NoticiaAdapter.java
@@ -1,11 +1,14 @@
 package com.example.estudiante.listasavanzadasjerry;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.estudiante.listasavanzadasjerry.Noticia;
 
@@ -45,7 +48,7 @@ public class NoticiaAdapter extends BaseAdapter {
 
     //generar un reglon por objeto
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
         //pasa un XML a View
         LayoutInflater inflater= activity.getLayoutInflater();
 
@@ -54,10 +57,23 @@ public class NoticiaAdapter extends BaseAdapter {
         TextView item_titulo = renglon.findViewById(R.id.item_titulo);
         TextView item_fecha = renglon.findViewById(R.id.item_fecha);
         TextView item_descripcion= renglon.findViewById(R.id.item_descripcion);
+        Button btn_like= renglon.findViewById(R.id.btn_like);
+        Button btn_dislike= renglon.findViewById(R.id.btn_dislike);
 
         item_descripcion.setText(noticias.get(position).getDescripcion());
         item_fecha.setText(noticias.get(position).getFecha());
         item_titulo.setText(noticias.get(position).getTitulo());
+
+        btn_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+              //  Toast.makeText(activity, noticias.get(position).getTitulo(), Toast.LENGTH_SHORT).show();
+                Intent intent= new Intent(activity, NoticiasView.class);
+                activity.startActivity(intent);
+
+            }
+        });
 
         return renglon;
     }
